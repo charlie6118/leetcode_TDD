315a4d19dc4cb02a160a38cc0174cda4c54a44fb
Shang Yi <shang.yi.yu.tw@gmail.com>
2019-05-07 19:19:19 +0800

    Refactor Add Two Numbers

--- AddTwoNumbers/src/solution.py
+++ AddTwoNumbers/src/solution.py
@@ -1,28 +1,26 @@
 # Definition for singly-linked list.
-class ListNode:
+class ListNode(object):
     def __init__(self, x):
         self.val = x
         self.next = None
 
-class Solution:
-    def addTwoNumbers(self, l1: ListNode, l2: ListNode) -> ListNode:
-        acc1 = 0
-        mu1 = 1
-        acc2 = 0
-        mu2 = 1
-        while l1:
-            acc1 += l1.val * mu1
-            mu1 = mu1 * 10
-            l1 = l1.next
-        while l2:
-            acc2 += l2.val * mu2
-            mu2 = mu2 * 10
-            l2 = l2.next
-        rs = str(acc1 + acc2)
-        print(rs)
-        result = head = ListNode(0)
-        for s in reversed(rs):
-            head.next = ListNode(int(s))
-            head = head.next
+class Solution(object):
+    def addTwoNumbers(self, l1, l2):
+        """
+        :type l1: ListNode
+        :type l2: ListNode
+        :rtype: ListNode
+        """
+        result = cur = ListNode(0)
+        c = 0
+        while l1 or l2 or c:
+            if l1:
+                c += l1.val
+                l1 = l1.next
+            if l2:
+                c += l2.val
+                l2 = l2.next
+            cur.next = ListNode(c % 10)
+            cur = cur.next
+            c = c // 10
         return result.next
-            
