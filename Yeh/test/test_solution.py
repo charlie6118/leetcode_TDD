567a3dd0e5f7b6109a861b7466f746d63b05abe8
Shang Yi <shang.yi.yu.tw@gmail.com>
2019-05-09 17:40:39 +0800

    rename

--- Yeh/test/test_solution.py
+++
@@ -1,7 +0,0 @@
-import pytest
-from src.solution import Solution
-
-s = Solution()
-
-def test_case0():
-    pass
--- Yeh/test/test_task1.py
+++
@@ -1,7 +0,0 @@
-import pytest
-from src.task1 import main
-
-s = Solution()
-
-def test_case0():
-    pass
